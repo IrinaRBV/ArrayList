4a69d1dec6bc51ff40efbccfa6a184eb86255748
Irina Ryabova <irina2013266@gmail.com>
2021-03-24 23:07:47 +0300

    Добавление аннотации @Nested and @DisplayName

--- src/test/java/ArrayListTest.java
+++ src/test/java/ArrayListTest.java
@@ -1,7 +1,6 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@DisplayName("test arrayList")
 public class ArrayListTest {
 
     private ArrayListInterface people;
@@ -15,50 +14,67 @@ public class ArrayListTest {
         }
     }
 
-    @Test
-    void getIncorrectIndexShouldBeException() {
-        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> {
-            people.get(10);
-        });
+    @Nested
+    @DisplayName("index check method")
+    class checkIndex{
+
+        @Test
+        @DisplayName("when incorrect index")
+        void getIncorrectIndexShouldBeException() {
+            Assertions.assertThrows(IndexOutOfBoundsException.class, () -> {
+                people.get(10);
+            });
+        }
+
+        @Test
+        @DisplayName("when correct index")
+        void getCorrectIndexShouldBeRightElement() {
+            Person person = people.get(3);
+            Assertions.assertEquals("Person3", person.getName());
+        }
+
+        @Test
+        @DisplayName("when get size list")
+        void getSizeListShouldBe10() {
+            Assertions.assertEquals(10, people.size());
+        }
     }
 
     @Test
-    void getCorrectIndexShouldBeRightElement() {
-        Person person = people.get(3);
-        Assertions.assertEquals("Person3", person.getName());
-    }
-
-    @Test
-    void getSizeListShouldBe10() {
-        Assertions.assertEquals(10, people.size());
-    }
-
-    @Test
+    @DisplayName("when clear list")
     void getClearListShouldBe0() {
         people.clear();
         Assertions.assertEquals(0, people.size());
     }
 
-    @Test
-    void whenRemoveByIndexSizeShouldBeDecreased() {
-        Assertions.assertTrue(people.removeByIndex(2));
-        Assertions.assertEquals(9, people.size());
-    }
+    @Nested
+    @DisplayName("change arrayList object method")
+    class changeListObject {
 
-    @Test
-    void whenRemoveByElementSizeShouldBeDecreased() {
-        Person person = new Person("Tom", 20, "Manager");
-        people.add(person);
-        Assertions.assertEquals(11, people.size());
-        Assertions.assertTrue(people.remove(person));
-        Assertions.assertEquals(10, people.size());
-    }
+        @Test
+        @DisplayName("when remove object by index in the list")
+        void whenRemoveByIndexSizeShouldBeDecreased() {
+            Assertions.assertTrue(people.removeByIndex(2));
+            Assertions.assertEquals(9, people.size());
+        }
 
-    @Test
-    void whenAddElementSizeShouldBeIncreased() {
-        Person person = new Person("Tom", 20, "Manager");
-        people.add(person);
-        Assertions.assertEquals(11, people.size());
+        @Test
+        @DisplayName("when remove object in the list")
+        void whenRemoveByElementSizeShouldBeDecreased() {
+            Person person = new Person("Tom", 20, "Manager");
+            people.add(person);
+            Assertions.assertEquals(11, people.size());
+            Assertions.assertTrue(people.remove(person));
+            Assertions.assertEquals(10, people.size());
+        }
+
+        @Test
+        @DisplayName("when add object in the list")
+        void whenAddElementSizeShouldBeIncreased() {
+            Person person = new Person("Tom", 20, "Manager");
+            people.add(person);
+            Assertions.assertEquals(11, people.size());
+        }
     }
 
 }
